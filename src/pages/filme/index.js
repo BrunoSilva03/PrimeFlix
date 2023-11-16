38f495fe5b706122571a5129aec8db814e96792a
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-11-16 15:53:04 -0300

    inserindo filme na lista de favoritos e outros

--- src/pages/filme/index.js
+++ src/pages/filme/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import api from '../../services/api'
 
@@ -7,6 +7,7 @@ import styles from './filme.module.css'
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -25,8 +26,10 @@ function Filme() {
                 //case de sucesso caso o filme exista!!!
             })
             .catch(() => {
-                console.log("FILME NÃO ENCONTRADO")
+                console.log("FILME NÃO ENCONTRADO");
                 //caso não encontre o filme
+                navigate("/", {replace: true});
+                return;
             })
         }
 
@@ -36,7 +39,41 @@ function Filme() {
         return () => {
             console.log("COMPONENTE FOI DESMONTADO");
         }
-    }, [])
+    }, [navigate, id]);
+
+
+    function salvarFilme() {
+        //nomeando a chave do meu localStorage como @primeflix
+        //e a mesma vai ter o retorno dentro dessa variável minhaLista
+        const minhaLista = localStorage.getItem("@primeflix");
+
+
+        //Se essa lista existe eu continuo com ela, senão eu inicio um array vazio.
+        //JSON.parse para converter em lista de novo porque quando envia uma lista tem que transformar em string.
+        //se tiver alguma coisa na minhaLista ele vai colocar na variável filmesSalvos, se não tiver ele vai vir como
+        //undefined e vai criar um array vazio.
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        //verificando se em filmesSalvos já tem o filme que você está escolhendo para salvar.
+        //se o id do filmeSalvo agora for igual ao id de algum filme já presente na state filme
+        //então não vai salvar o filme de novo repetidamente uai
+        const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id)
+
+        if(hasFilme) {
+            alert('Este filme já está salvo nos seus favoritos!!!');
+            return;
+        }
+
+        //colocando mais um item no filmesSalvos, o filme que é o objeto que está no nosso useState
+        filmesSalvos.push(filme);
+
+        //agora salvando um filme no localStorage, primeiro passa a chave que declaramos lá em cima,
+        //depois JSON.stringify pra transformar pra uma string porque não conseguimos salvar um array
+        //no localStorage
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        alert("FILME SALVO COM SUCESSO!!!");
+
+    }
 
 
     if(loading) {
@@ -58,9 +95,9 @@ function Filme() {
                 <strong>Avaliação: {filme.vote_average} / 10</strong>
 
                 <div className={styles.areaButtons}>
-                    <button>Salvar</button>
+                    <button onClick={salvarFilme}>Salvar</button>
                     <button>
-                        <a href="#">Trailer</a>
+                        <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title}`}>Trailer</a>
                     </button>
                 </div>
             </div>
